a30f2e86ca60fd6e94288fc07df16d6cf5b79b72
roundcone <info@roundcone.com>
2015-03-04 14:01:29 +0100

    BreezingForms
    styles voor contactpagina - formulieren opgemaakt in BreezinForms

---
+++ media/breezingforms/facileforms.config.php
@@ -0,0 +1,31 @@
+<?php
+$this->stylesheet = "1";
+$this->wysiwyg = "0";
+$this->areasmall = "4";
+$this->areamedium = "12";
+$this->arealarge = "20";
+$this->limitdesc = "100";
+$this->emailadr = "Enter your email address here";
+$this->images = "{mossite}/components/com_breezingforms/images";
+$this->uploads = "{mospath}/media/breezingforms/uploads";
+$this->movepixels = "10";
+$this->compress = "1";
+$this->livesite = "0";
+$this->getprovider = "0";
+$this->gridshow = "1";
+$this->gridsize = "10";
+$this->gridcolor1 = "#e0e0ff";
+$this->gridcolor2 = "#ffe0e0";
+$this->viewed = "0";
+$this->exported = "0";
+$this->archived = "0";
+$this->formname = "";
+$this->menupkg = "";
+$this->formpkg = "QuickModeForms";
+$this->scriptpkg = "";
+$this->piecepkg = "";
+$this->csvdelimiter = ";";
+$this->csvquote = "\"";
+$this->cellnewline = "1";
+$this->enable_classic = "0";
+?>
